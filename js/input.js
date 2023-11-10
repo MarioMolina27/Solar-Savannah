5b734aa8a874ad6fdbd9d08f767838d438c484c1
Mario Molina <molinaballesteros@hotmail.com>
2023-11-10 19:59:32 +0100

    añadido personaje game proposal indicaciones

--- js/input.js
+++ js/input.js
@@ -15,21 +15,25 @@ export function inputDetected(event,pacman) {
     }
     switch (event.key) {
         case teclasFlecha.LEFT:
+            document.getElementById('speech-txt').innerHTML = "Left";
             interval = setInterval(function () {
                 pacman.directionChange('left', false);
             }, time); // El valor 300 representa 0.3 segundos en milisegundos
             break;
         case teclasFlecha.RIGHT:
+            document.getElementById('speech-txt').innerHTML = "Right";
             interval = setInterval(function () {
                 pacman.directionChange('right', false);
             }, time);
             break;
         case teclasFlecha.UP:
+            document.getElementById('speech-txt').innerHTML = "Up";
             interval = setInterval(function () {
                 pacman.directionChange('up', false);
             }, time);
             break;
         case teclasFlecha.DOWN:
+            document.getElementById('speech-txt').innerHTML = "Down";
             interval = setInterval(function () {
                 pacman.directionChange('down', false);
             }, time);
