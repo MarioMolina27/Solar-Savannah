3590bc95dba3a086142ff3f5571f21837833222e
Mario Molina <molinaballesteros@hotmail.com>
2023-11-29 19:37:08 +0100

    añadido boton play antes de jugar

--- js/main.js
+++ js/main.js
@@ -10,34 +10,49 @@ export let gameLoopInterval;
 export let timerInterval;
 export const gameBoard = new Board(config.layout, config.grid, new Pacman(), utilities.findEnemies());
 
-gameBoard.pacman.createHearts();
-gameBoard.printBoard();
+export let totalPoints;
+export let totalPowerUps;
+
+let btnPlay = document.querySelector(".play-game-btn");
+let gameContainer = document.querySelector(".game-container");
+let speechBubble = document.getElementById("speech-txt");
 
 
-let items = utilities.countItems();
-export const totalPoints = items[0];
-export const totalPowerUps = items[1];
+btnPlay.addEventListener("click", function()
+{
+    speechBubble.innerText = "Començem!";
+    gameContainer.style.display = "flex";
+    gameBoard.pacman.createHearts();
+
+    gameBoard.printBoard();
 
 
-console.log("puntos necesarios: "+items[0]);
-console.log("power necesarios: "+items[1]);
+    let items = utilities.countItems();
+    totalPoints = items[0];
+    totalPowerUps = items[1];
 
-document.addEventListener('keydown', function(event) {
-    inputDetected(event, gameBoard.pacman);
+
+    console.log("puntos necesarios: "+items[0]);
+    console.log("power necesarios: "+items[1]);
+
+    document.addEventListener('keydown', function(event) {
+        inputDetected(event, gameBoard.pacman);
+    });
+
+
+    function gameLoop()
+    {
+        gameBoard.enemies.forEach(enemie => {
+            if(enemie.isAlive)
+            {
+                enemie.moveRandomly();
+            }
+        });
+        gameBoard.printBoard();
+    }
+
+
+    gameLoopInterval = setInterval(gameLoop,config.gameLoopDuration);
+    timerInterval = setInterval(startTimer,config.gameLoopDuration);
 });
 
-
-function gameLoop()
-{
-    gameBoard.enemies.forEach(enemie => {
-        if(enemie.isAlive)
-        {
-            enemie.moveRandomly();
-        }
-    });
-    gameBoard.printBoard();
-}
-
-
-gameLoopInterval = setInterval(gameLoop,config.gameLoopDuration);
-timerInterval = setInterval(startTimer,config.gameLoopDuration);
